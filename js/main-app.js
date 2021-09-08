2e0ee518b78200f4c4b0187ccf3bd576c6390a30
Wallwrecker <sinantakboy@gmail.com>
2021-09-09 01:47:13 +0800

    done adding basic functionalities

--- js/main-app.js
+++ js/main-app.js
@@ -1,9 +1,4 @@
-// This will be used for temporary data handler later.
-const temporaryDataHandler = {title: "", type: "", url: "", description: "", date_posted: ""};
 
-// Initiate other essential elements from form inputs 
-const characterCount = document.getElementById("character-count");
-console.log(typeof characterCount.textContent)
 // Initiate necessary forminputs ID for attaching eventlistener later.
 const collectionOfFormInputId = [
   "subject-title",
@@ -11,7 +6,10 @@ const collectionOfFormInputId = [
   'subject-url',
   'subject-description'
 ];
+// This will be used for temporary data handler later.
+const temporaryDataHandler = {title: "", type: "", url: "", description: "", date_posted: ""};
 
+// Loops through all form elements and them appropriate eventListeners.
 collectionOfFormInputId.forEach(function(id) {
   const formInput = document.getElementById(id);
   
@@ -22,9 +20,10 @@ collectionOfFormInputId.forEach(function(id) {
       formInput.addEventListener('blur', function() {
         // But first let's verify if the input is legit.
         if(verifyFormTextbox(this)) {
+          // Get the data-key attribute as a basis key on temporary handler.
+          const dataKey = this.dataset.key;
           // Add value to the temporaryDataHandler varible.
-          const dataKey = this.id;
-          console.log(dataKey)
+          temporaryDataHandler[dataKey] = this.value;
         } else {
           // Then send some notification next to the input element.
           const notificationElement = this.nextElementSibling;
@@ -37,9 +36,9 @@ collectionOfFormInputId.forEach(function(id) {
       formInput.addEventListener('change', function() {
         // But first let's verify if the input is legit.
         if(verifyFormTextbox(this)) {
-          const dataKey = this.dataset.key;
+          // If there is no error then,
           // Add value to the temporaryDataHandler object varible.
-          console.log(dataKey);
+          saveToDataHandler(this.dataset.key, this.value)
         } else {
           // Then send some notification next to the input element.
           const notificationElement = this.nextElementSibling;
@@ -49,28 +48,49 @@ collectionOfFormInputId.forEach(function(id) {
       break;
     
     case "textarea":
-      let mydescription = "";
+      const maximumCharacterCount = 130; 
+      let descriptionHandler = "";
+     
       /*
         This element it has to be a limit of how many characters
         that a user can enter and that limit is 150 characters.
         That's what the eventlistener below purpose. To monitor how many characters
         and to limit.
       */
-      formInput.addEventListener('change',function() {
-        
+      formInput.addEventListener('input',function(e) {
+        // Initiate other essential elements from form inputs 
+        const characterCounterElement = document.getElementById("character-count");
+        characterCounterElement.textContent = this.value.length;
+        descriptionHandler = this.value;
       });
 
+      // Then after it lose focus to the textarea save it to the
+      // temporarydatahandler object.
       formInput.addEventListener('blur', function() {
-       
+        saveToDataHandler(this.dataset.key, descriptionHandler);
       });
   }
 });
 
+const submitButton = document.getElementById('submit-button');
+submitButton.addEventListener('click', function() {
+  console.log(temporaryDataHandler);
+  const currentDate = new Date()
+})
+
+
+
+// Tidbits functions
 // Function to verify input & select elements value
 function verifyFormTextbox(element) {
-  if(element.value == 0 || element.value.length === 0 || element.value == "0") { return false;} return true;
+  if(element.value == 0 || element.value.length === 0 || element.value == "0") {return false;} return true;
 }
 
 function verifySelectElement(element) {
   if(selectElement.value == "0" || selectElement.value === 0) {return false;} return true;
+}
+
+// Function to save form input data. 
+function saveToDataHandler(dataKey, data) {
+  temporaryDataHandler[dataKey] = data;
 }
