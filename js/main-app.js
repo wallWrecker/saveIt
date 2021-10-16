2137e24e728d20788531a089819c85e6e3490a5f
wallWrecker <sinantakboy@gmail.com>
2021-10-16 22:45:02 +0800

    Done creating validation for multiple input fields

--- js/main-app.js
+++ js/main-app.js
@@ -1,43 +1,49 @@
 // Initialize collections object.
 // Get the collection form localstorage if none then it returns a new empty array.
-const collectionObject = JSON.parse(localStorage.getItem('collections')) || [];
+const collectionObject = JSON.parse(localStorage.getItem("collections")) || [];
 
 // This will be used for temporary data handler later.
-const temporaryDataHandler = {title: "", type: "", url: "", description: "", date_posted: ""};
+const temporaryDataHandler = {
+  title: "",
+  type: "",
+  url: "",
+  description: "",
+  date_posted: "",
+};
 // Initiate necessary forminputs ID for attaching eventlistener later.
 const collectionOfFormInputId = [
   "subject-title",
   "subject-type",
-  'subject-url',
-  'subject-description'
+  "subject-url",
+  "subject-description",
 ];
 
-// Loops all inputs and select elements, 
+// Loops all inputs and select elements,
 // and apply some event listener.
-collectionOfFormInputId.forEach(function(id) {
+collectionOfFormInputId.forEach(function (id) {
   const formInput = document.getElementById(id);
 
-  switch(formInput.localName) {
+  switch (formInput.localName) {
     case "input":
       // Attatch event listener.
-      formInput.addEventListener('blur', function() {
-        if(validateTextboxValue(this)) {
+      formInput.addEventListener("blur", function () {
+        if (validateTextboxValue(this)) {
           approvedInputField(this.id);
           /*using data attribute attatched to the 
-            input fields or select field as a object property name. */ 
-          const dataKey = this.dataset.key; 
+            input fields or select field as a object property name. */
+          const dataKey = this.dataset.key;
           temporaryDataHandler[dataKey] = this.value;
         } else {
           // This is to inform the user that he/she has selected the wrong item.
           deniedInputField(this.id);
         }
       });
-    break;
+      break;
 
     case "select":
-      formInput.addEventListener('change', function() {
+      formInput.addEventListener("change", function () {
         // But first let's verify if the input is legit.
-        if(validateSelectedType(this)) {
+        if (validateSelectedType(this)) {
           approvedInputField(this.id, "I think it was nice choice.");
           // If there is no error then,
           // Add value to the temporaryDataHandler object varible.
@@ -45,26 +51,26 @@ collectionOfFormInputId.forEach(function(id) {
         } else {
           deniedInputField(this.id);
         }
-      })
+      });
       break;
-    
+
     case "textarea":
       let descriptionHandler = "";
       /*This element it has to be a limit of how many characters
         that a user can enter and that limit is 150 characters.
         That's what the eventlistener below purpose. To monitor how many characters
-        and to limit. */ 
-      formInput.addEventListener('input', function(e) {
+        and to limit. */
+      formInput.addEventListener("input", function (e) {
         // output character counts based on how many character are there in
-        document.getElementById("character-count").textContent  = this.value.length;
+        document.getElementById("character-count").textContent = this.value.length;
         // Save the current text from textarea to description handler variable.
         descriptionHandler = this.value;
       });
 
       // Then after it lose focus to the textarea save it to the
       // temporarydatahandler object.
-      formInput.addEventListener('blur', function() {
-        if(validateTextboxValue(this)) {
+      formInput.addEventListener("blur", function () {
+        if (validateTextboxValue(this)) {
           approvedInputField(this.id);
           saveToDataHandler(this.dataset.key, descriptionHandler);
         } else {
@@ -74,41 +80,55 @@ collectionOfFormInputId.forEach(function(id) {
   }
 });
 
-// Events for submit button..
-const submitButton = document.getElementById('submit-button');
-submitButton.addEventListener('click', function() {
-  // !!! NOTE: DAPAT SA INPUT FIELDS TAYO MAG CHECK,
-  // !!! HINDI SA TDH PROPPERTIES.
-  
-  const responseArray = [];
-  for(let property in temporaryDataHandler) {
-    // TDH shorthand for temporaryDataHandler also to make it shorther.
-    const TDH = temporaryDataHandler;
-    if(TDH[property] == "") {
-      if(property != "date_posted") {
-        // It will produce an error-alert the user. 
-        // Produce an alert to the input field related to property.
-        responseArray.push(false);
-        deniedInputField(`subject-${property}`);
-      } else {
-        // I will just assign the date of creation.
-        TDH[property] = getTodayDate();
-        responseArray.push(true);
-      }
-    } else {
-      approvedInputField(`subject-${property}`)
-      responseArray.push(true);
-    }
+const submitButton = document.getElementById("submit-button");
+
+submitButton.addEventListener("click", function () {
+  const { result, fields } = validateMultipleInputFields(collectionOfFormInputId);
+  console.log(Array.isArray(fields));
+  if (result) {
+    temporaryDataHandler["date_posted"] = getTodayDate();
+    collectionObject.push(temporaryDataHandler);
+  } else {
+    fields.forEach(inputfield => {
+      
+    });
   }
-  // If anyone has all have a value.
-  // Then it will be the time to save the input.
-  console.log("Response are: " + responseArray);
-  // If (all are have) {}
-})
+});
 
 // ### FUNCTIONS ####
 // Tidbits functions
-// Function to verify input & select elements.
+function validateMultipleInputFields(inputIdArray) {
+  const response = [];
+  const collectionOfInputFieldsWithIssue = [];
+
+  inputIdArray.forEach(function (id) {
+    const formElement = document.getElementById(id);
+    const inputLocalName = formElement.localName;
+    if (inputLocalName == "input" || inputLocalName == "textarea") {
+      if (validateTextboxValue(formElement) == true) {
+        response.push(true);
+      } else {
+        response.push(false);
+        collectionOfInputFieldsWithIssue.push(formElement);
+      }
+    } else if (inputLocalName == "select") {
+      if (validateSelectedType(formElement) == true) {
+        response.push(true);
+      } else {
+        response.push(false);
+        collectionOfInputFieldsWithIssue.push(formElement);
+      }
+    }
+  });
+
+  return {
+    result: response.every(function (response) {
+      response == true;
+    }),
+    fields: collectionOfInputFieldsWithIssue,
+  };
+}
+// Function to verify individual input & select elements.
 
 // validateTextboxValue to now validateTextboxValue
 function validateTextboxValue(textbox) {
@@ -131,9 +151,8 @@ function getTodayDate() {
   const day   = d.getDate();
   const month = d.getMonth();
   const year  = d.getFullYear();
-
-  currentDate = `${month}-${day}-${year}`;
-  return currentDate;
+  
+  return `${month}-${day}-${year}`;
 }
 
 // Function to convert & save temporary data object to collections object.
@@ -149,48 +168,49 @@ function saveToLocalStorage(temporaryHandler) {
 // Functions to invoke and or revoke input forms notifications.
 function deniedInputField(id, customMessage) {
   const inputfield = document.getElementById(id);
-  inputfield.classList.remove('warning-border', 'error-border', 'success-border');
-  
+  inputfield.classList.remove(
+    "warning-border",
+    "error-border",
+    "success-border"
+  );
+
   const inputFieldMessage = inputfield.nextElementSibling;
-  inputfield.classList.add("error-border"); 
-  
+  inputfield.classList.add("error-border");
+
   inputFieldMessage.innerText = customMessage || "Hey! I think you need to fill this up!";
   inputFieldMessage.style.color = "rgb(255, 38, 38)";
   console.log(inputfield);
 }
 
-// function warnedInput(id, message) {
-//   const inputfield = document.getElementById(id);
-//   const inputFieldMessage = inputfield.nextElementSibling;
-  
-//   inputfield.classList.add("warning-border");
-  
-//   inputFieldMessage.innerText = message || "Hey! I think you need to fill this up!";
-//   inputFieldMessage.style.color = "rgb(252, 84, 4)";
-// }
-
 function approvedInputField(id, message) {
   const inputfield = document.getElementById(id);
-  inputfield.classList.remove('warning-border', 'error-border', 'success-border');
-
+  inputfield.classList.remove(
+    "warning-border",
+    "error-border",
+    "success-border"
+  );
   const inputFieldMessage = inputfield.nextElementSibling;
   inputfield.classList.add("success-border");
   inputFieldMessage.innerText = message || "This was ok.";
   inputFieldMessage.style.color = "rgb(41, 187, 137)";
 }
 function removeErrorsWarnings(id) {
-  const inputField = document.getElementById(id); 
-  
-  if(inputField.classList.contains('warning-border') || inputField.classList.contains('error-border')) {
-    // removes both classnames even if they are 
-    inputField.classList.remove('warning-border', 'error-border', 'success-border');
+  const inputField = document.getElementById(id);
+
+  if (inputField.classList.contains("warning-border") || inputField.classList.contains("error-border")) {
+    // removes both classnames even if they are
+    inputField.classList.remove(
+      "warning-border",
+      "error-border",
+      "success-border"
+    );
     const inputFieldMessage = inputField.nextElementSibling;
-    const markupName = inputField.localName; 
-    
+    const markupName = inputField.localName;
+
     // For the assigning the of message
-    if(markupName == 'input' || markupName == 'textarea') {
+    if (markupName == "input" || markupName == "textarea") {
       inputFieldMessage.textContent = "Please fill the input field.";
-    } else if(markupName == 'select') {
+    } else if (markupName == "select") {
       inputFieldMessage.textContent = "Please select type of your subject.";
     }
 
@@ -203,5 +223,5 @@ function removeErrorsWarnings(id) {
 
 // Functions to custom some element
 function changeFontColor(element, colorOfChoice) {
-  return element.style.color = `${colorOfChoice}`;
+  return (element.style.color = `${colorOfChoice}`);
 }
