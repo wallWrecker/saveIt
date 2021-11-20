2313e5afa363e7ab9beb0303e56000fcd9701ef3
wallwrecker <sinantakboy@gmail.com>
2021-11-20 13:45:22 +0800

    Coded initial function components.

--- js/components/story-component/date_time-component.js
+++ js/components/story-component/date_time-component.js
@@ -5,18 +5,31 @@ const time_posted_component = function(time) {
   
   time_container.classList.add('text-muted', 'ms-1');
   
-  icon_span.classList.add('bi', 'bi-calendar2-event');
-  icon_span.classList('text-muted', 'ms-1');
+  icon_span.classList.add('bi', 'bi-clock', 'fs-6');
   
   time_container.appendChild(icon_span);
   time_container.appendChild(theTime);
   return time_container;
 }
 
-const date_posted_component = function () {
+const date_posted_component = function (date) {
+  let date_container = document.createElement('div');
+  let icon_span = document.createElement('span');
+  let theDate = document.createTextNode("Nov 29,2019");
   
+  date_container.classList.add('text-muted', 'ms-1');
+  
+  icon_span.classList.add('bi', 'bi-calendar2-event','pe-1');
+  
+  date_container.appendChild(icon_span);
+  date_container.appendChild(theDate);
+  return date_container;
 }
 
-const date_time_component = function() {
-  
+export const date_time_component = function({date, time}) {
+  const date_time_container = document.createElement('div');
+  component_container.classList.add("d-flex", "d-sm-column");
+  component_container.appendChild(date_posted_component(date));
+  component_container.appendChild(time_posted_component(time));
+  return date_time_container;
 }
