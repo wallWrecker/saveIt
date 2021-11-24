008b571116c68a0ec919cd77c07cf020e198320b
wallwrecker <sinantakboy@gmail.com>
2021-11-24 20:57:08 +0800

    created a component prototype [untested]

---
+++ js/components/story-component/story-component.js
@@ -0,0 +1,26 @@
+import { posted_type_component } from "./posted-type-component.js";
+import {content_body_component} from "./content_body_component.js";
+import {date_time_component} from "./date_time-component.js"
+
+// What date that the story component get?
+function story_component(dataObj) {
+  const {id, url, title, type, date, time, description} = dataObj;
+  const story_component_container = documet.createElement('div');
+  story_component_container.classList.add('col-sm-5', 'col-md-5', 'col-lg-4', 'col-xl-3', 'rounded-2', 'shadow-sm', 'bg-white', 'p-3', 'm-1', 'h-auto');
+  const childElements = [
+    posted_type_component(type),
+    content_body_component(
+      {
+        title: title,
+        description: description,
+      }
+    ),
+    date_time_component(
+      {
+        date: date,
+        time: time
+      }
+    )
+  ];
+  story_component_container.append([childElements])
+}
