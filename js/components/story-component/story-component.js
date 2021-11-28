2be89e21243df0adf85a627ef4ff597ca10fdfab
wallwrecker <sinantakboy@gmail.com>
2021-11-28 12:40:05 +0800

    create prototype story-component

--- js/components/story-component/story-component.js
+++ js/components/story-component/story-component.js
@@ -1,16 +1,20 @@
 import { posted_type_component } from "./posted-type-component.js";
-import {content_body_component} from "./content_body_component.js";
-import {date_time_component} from "./date_time-component.js"
+import { content_body_component } from "./content_body_component.js";
+import { date_time_component } from "./date_time-component.js"
 
 // What date that the story component get?
-function story_component(dataObj) {
+export function story_component(dataObj) {
   const {id, url, title, type, date, time, description} = dataObj;
-  const story_component_container = documet.createElement('div');
-  story_component_container.classList.add('col-sm-5', 'col-md-5', 'col-lg-4', 'col-xl-3', 'rounded-2', 'shadow-sm', 'bg-white', 'p-3', 'm-1', 'h-auto');
+  const story_component_container = document.createElement('div');
+  
+  const element_classes = ['col-sm-5', 'col-md-5', 'col-lg-4', 'col-xl-3', 'rounded-2', 'shadow-sm', 'bg-white', 'p-3', 'm-1', 'h-auto'];
+  story_component_container.classList.add(element_classes);
+  
   const childElements = [
     posted_type_component(type),
     content_body_component({title: title, description: description}),
     date_time_component({date: date,time: time})
   ];
-  story_component_container.append([childElements])
+
+  story_component_container.append([childElements]);
 }
