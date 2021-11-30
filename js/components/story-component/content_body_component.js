726916d99a60336eed9b3025fbeacbec486414e0
wallwrecker <sinantakboy@gmail.com>
2021-11-30 15:39:40 +0800

    Changed append content to excecutable function components

--- js/components/story-component/content_body_component.js
+++ js/components/story-component/content_body_component.js
@@ -1,21 +1,20 @@
 function content_title_component(title) {
   const h5 = document.createElement('h5');
-  const title = document.createTextNode(title);
   h5.classList.add('mt-0');
-  h5.appendChild(title);
+  h5.textContent = title;
   return h5;
 }
 
 function content_description_component(description) {
   const description_container = document.createElement('p');
-  const description = document.createTextNode(description);
+  const myDescription = document.createTextNode(description);
   description_container.classList.add('text-muted');
-  description_container.appendChild(description);
+  description_container.appendChild(myDescription);
   return description_container;
 }
 
 export function content_body_component({title, description}) {
   const content_body_container = document.createElement('div');
-  content_body_container.append([content_title_component, content_description_component]);
+  content_body_container.append(content_title_component(title), content_description_component(description));
   return content_body_container;
 }
